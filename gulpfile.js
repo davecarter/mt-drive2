a745359c539e415e31a3f8ff30a91a25fc73370a
Nahuel Sotelo <nahuelsotelo@gmail.com>
2015-08-21 13:29:05 +0200

    prevent legacy files to compile

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,8 @@ var gulp = require('gulp'),
     svgmin = require('gulp-svgmin'),
 
     ghPages = require('gulp-gh-pages'),
+    slack = require('gulp-slack'),
+
     yaml = require('gulp-yaml');
 
 
@@ -161,7 +163,14 @@ var gulp = require('gulp'),
 // DEPLOY ====================================================================
   gulp.task('upload', ['build'], function() {
     return gulp.src( basePath.dist + '/**/*')
-      .pipe(ghPages());
+      .pipe(ghPages())
+      // .pipe(slack({
+      //   url: '*Your Webhook URL*',
+      //   channel: '#foo', // Optional
+      //   user: 'bar', // Optional
+      //   icon_url: 'http://foo.com/bar.jpg', // Optional
+      //   icon_emoji: ':bowtie:' // Optional
+      // });
   });
 
   gulp.task('deploy', ['upload'], function(callback) {
